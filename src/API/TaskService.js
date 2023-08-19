435022f922f0fa60edc6b9c1284d1d082330f759
ArseniyK1 <qwertototo2332@gmail.com>
2023-08-19 17:25:13 +0300

    rewrite tsksvc

--- src/API/TaskService.js
+++ src/API/TaskService.js
@@ -34,12 +34,6 @@ export default class TasksService {
       });
       return response;
     } catch (error) {
-      if (error.response.status === 403) {
-        alert(
-          "Слишком много запросов, не удалось загрузить количество репозиториев, попробуйте позже."
-        );
-        return;
-      }
       console.error(error);
     }
   }
